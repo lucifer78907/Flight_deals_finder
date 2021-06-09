fbecf8c1c970abd164ef8a2450c28fc9d38fd242
Rudra <chaudhary66667@gmail.com>
2021-06-09 22:24:42 +0530

    Working data manager

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,11 @@
+import requests
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.sheety_url="https://api.sheety.co/f51728668cc6d9f9988bb2e676485cd7/myFlightDeals/prices"
+        self.response=requests.get(url=self.sheety_url)
+        self.response.raise_for_status()
+
+    def get_data(self):
+        self.my_data=self.response.json()
+        return self.my_data
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from pprint import pprint
+data=DataManager()
+sheet_data=data.get_data()
+pprint(sheet_data)
