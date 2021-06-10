840f0c6b56b89ed7e88b943b4d4cb0c101837812
Rudra <chaudhary66667@gmail.com>
2021-06-10 10:33:18 +0530

    Finalized notification manager

--- notification_manager.py
+++ notification_manager.py
@@ -7,13 +7,13 @@ class NotificationManager:
         self.account_sid = "AC682f47acde6b0e9200f2e76c183bae7d"
         self.auth_token = "e730072a6ffdfd2fcd67bbc0c689084a"
 
-    def send_message(self,price,city_from,city_to,city_to_iata):
+    def send_message(self,price,city_from,city_to,city_to_iata,dp_date):
         client = Client(self.account_sid, self.auth_token)
 
         message = client.messages \
             .create(
             body=f"Low price Alert only ${price} \n from {city_from} to "
-                 f"{city_to}-{city_to_iata}\n on ",
+                 f"{city_to}-{city_to_iata}\n on {dp_date}",
             from_='+18563475338',
             to='+918171768882'
         )
