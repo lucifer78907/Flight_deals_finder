794994b45832d8ddf00ed6da93afe17c46674b8b
Rudra <chaudhary66667@gmail.com>
2021-06-10 09:00:31 +0530

    added working of flight data

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,34 @@
+import datetime as dt
+
+import requests
+
+API_ENDPOINT_URL="https://tequila-api.kiwi.com/v2/search"
+API_KEY="nQl0yIRUBg8IHjngw7eirQxfeiqZSvgY"
+header_param={
+    "apikey":API_KEY
+}
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+
+    def __init__(self):
+        self.price=0
+        self.departure_airport_code="LON"
+        self.departure_city="London"
+
+    def set_time(self):
+        self.today=dt.datetime.now()
+        self.from_date=self.today.strftime("%d/%m/%Y")
+        self.date_range=self.today+dt.timedelta(days=180)
+        self.to_date=self.date_range.strftime("%d/%m/%Y")
+
+    def print_data(self,city):
+        self.user_param = {
+            "fly_from": self.departure_airport_code,
+            "fly_to": city,
+            "date_from":self.from_date,
+            "date_to":self.to_date,
+            "curr":"USD"
+        }
+        response=requests.get(url=API_ENDPOINT_URL,params=self.user_param,headers=header_param)
+        data=response.json()
+        new_data=data['data'][0]
+        print(city,f" ${new_data['price']}")
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from data_manager import DataManager
-
+from flight_data import FlightData
 data = DataManager()
 sheet_data = data.get_data()
+my_flight_data=FlightData()
+my_flight_data.set_time()
 # check the sheet data for empty iata codes
 for elements in sheet_data:
     if elements['iataCode'] == '':
@@ -17,3 +19,6 @@ for elements in sheet_data:
         }
         ele_id = elements['id']
         data.update_iata_code(ele_dict, ele_id)
+
+for elements in sheet_data:
+        my_flight_data.print_data(elements['iataCode'])
